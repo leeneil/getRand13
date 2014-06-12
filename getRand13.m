7da3588e79f6c0e1596dd9e5f031892b5854cc91
Po-Nan Li <lishantreasure@gmail.com>
2014-06-12 11:45:11 +0800

    remove commented lines

--- getRand13.m
+++ getRand13.m
@@ -1,16 +1,12 @@
 function X = getRand13(~)
 
-% X = ceil( 13/110 * ( 10*(getRand10()+1) + (getRand10()+1)  ) ) - 1;
-% X = ( getRand10()+0 ) + 10*( getRand10()+0) + 100*( getRand10()+0);
-% X = floor( X / 999 * 13 ) + 0;
 
 X = 100*(getRand10()+1) + 10*(getRand10()+1) + 1*(getRand10()+1);
 
 X = X - 111;
 X = X / 999;
 X = X * 13;
-% X = X - 0.1;
-% X = X / 0.9 * 13;
+
 X = ceil(X) - 1;
 if X <0
     X = 0;
